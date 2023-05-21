fd73f89081db8163138facd4777065819cb6de26
NAFIA Salma <102663462+Nafia-Salma@users.noreply.github.com>
2023-05-21 16:08:52 +0100

    Update StorUser.php

--- app/Http/Requests/StorUser.php
+++ app/Http/Requests/StorUser.php
@@ -19,6 +19,8 @@ class StorUser extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
+     *
+     *
      * @return array
      */
     public function rules()
