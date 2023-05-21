e3a783114f846d1b20ca5768f66751dec82d564c
Laamiri-Fouzia <laamiri.f.fouzia@gmail.Com>
2023-05-21 16:36:32 +0100

    ajouter test add client

---
+++ tests/Unit/Test_AjoutClient.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AuthenticatedTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /**
+     * Test a function that requires authentication.
+     *
+     * @return void
+     */
+    public function testAuthenticatedFunction()
+    {
+        // Create and authenticate a user
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        // Make a request to the authenticated function
+        $response = $this->get('/clients');
+
+        // Assert the expected behavior based on the response
+        $response->assertStatus(200);
+        // Add more assertions as needed
+    }
+}
